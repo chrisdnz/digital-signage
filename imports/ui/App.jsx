21049f87ea2083e2d962b162d9c1202fe74b67e8
Óscar Sánchez <oscarsanchez@MacBook-Pro-de-Oscar.local>
2017-01-08 00:01:04 -0600

    minus changes

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,13 +6,13 @@ import Carousel from 'nuka-carousel';
 export default class App extends Component {
   constructor(props) {
       super(props);
-      
+
   }
 
   render() {
     return (
       <div className="container">
-        <Carousel autoplay={true} slideWidth={1} autoplayInterval={5000} dragging={true} wrapAround={true}>
+        <Carousel autoplay={true} slideWidth={1} autoplayInterval={5000} wrapAround={true}>
         <img src="http://localhost:3000/cfs/files/Images/vy9G27Had49EywjGb"/>
         <img src="http://localhost:3000/cfs/files/Images/LFyemnydnrwMbfwDs"/>
         <img src="http://localhost:3000/cfs/files/Images/n3MDBdWipTXNpWdCQ"/>
