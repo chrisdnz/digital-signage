bafda704189e7cb678333637d2a139fa0564e94f
Óscar Sánchez <oscarsanchez@MacBook-Pro-de-Oscar.local>
2017-01-31 20:14:54 -0600

    implementando nuevo carousel, falta resolver error con jquery's

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,29 +1,40 @@
 import React, { Component } from 'react';
-import Carousel from 'nuka-carousel';
+//import Carousel from 'nuka-carousel';
+ import './carousel';
 import { createContainer } from 'meteor/react-meteor-data';
 
 class App extends Component {
   constructor(props) {
     super(props);
   }
+  
   render() {
+    let i=0;
     return (
       <div className="container">
-        <Carousel speed={1000} easing="linear" autoplay={true} slideWidth={1} autoplayInterval={this.props.ads.map(ad => ad.timeOut)} wrapAround={true} decorators={[]}>
+        {/* <Carousel speed={1000} easing="linear" autoplay={true} slideWidth={1} autoplayInterval={this.props.ads.map(ad => ad.timeOut)} wrapAround={true} decorators={[]}>
           {this.props.codigos.map(codigo => <img  onLoad={() => {window.dispatchEvent(new Event('resize'));}} src={`http://localhost:4000/cfs/files/Images/${codigo.Codigo}`} key={codigo._id}/>)}
-        </Carousel>
-      </div>
-    );
+        </Carousel> */}
+        <div id="myCarousel"  className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner" role="listbox">
+            <div  className="item active" data-interval="1000">
+              <img src="https://dummyimage.com/600x400/000/fff" />
+              </div>
+              {this.props.codigos.map(codigo => <div  ref= {'photo'+(i++)} key={codigo._id} className="item"  data-interval={codigo.Time}><img  src={`http://localhost:4000/cfs/files/Images/${codigo.Codigo}`} /> </div>)}
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
-}
 
-export default createContainer(props => {
-  let data = conn.subscribe("codigos");
-  let time = conn.subscribe("ads");
-  return {
-    codigos: Codigos.find().fetch(),
-    ads: Ad.find({}).fetch(),
-    ready: data.ready(),
-    adready: time.ready()
-  }
-}, App)
+  export default createContainer(props => {
+    let data = conn.subscribe("codigos");
+    let time = conn.subscribe("ads");
+    return {
+      codigos: Codigos.find().fetch(),
+      ads: Ad.find({}).fetch(),
+      ready: data.ready(),
+      adready: time.ready()
+    }
+  }, App)
---
+++ imports/ui/carousel.js
@@ -0,0 +1,12 @@
+var t;
+
+var start = $('#myCarousel').find('.active').attr('data-interval');
+t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
+
+$('#myCarousel').on('slid.bs.carousel', function () {
+     clearTimeout(t);
+     var duration = $(this).find('.active').attr('data-interval');
+
+     $('#myCarousel').carousel('pause');
+     t = setTimeout("$('#myCarousel').carousel();", duration-1000);
+})
