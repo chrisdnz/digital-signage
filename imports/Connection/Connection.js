c73353cae706ea805266bb52ee48b82c51909f93
Christian Najera <chris_najera@unitec.edu>
2017-02-08 14:36:36 -0600

    Merge conflict fixed

--- imports/Connection/Connection.js
+++ imports/Connection/Connection.js
@@ -1,3 +1,3 @@
-conn = DDP.connect("http://unitec-anuncios.herokuapp.com/");
+conn = DDP.connect("https://unitec-anuncios.herokuapp.com/");
 Codigos = new Mongo.Collection("Codigos", conn);
 Ad = new Mongo.Collection('ads',conn);
